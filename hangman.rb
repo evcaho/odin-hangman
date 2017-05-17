7d0c6adc90f226fd1934fa7083c727e1df4550fa
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-17 15:07:27 -0700

    file writes clean upon lose

--- hangman.rb
+++ hangman.rb
@@ -10,17 +10,48 @@ def store_name(filename, string)
 	end
 end
 
+def empty_file(filename)
+	File.open(filename, "w")
+end
+
 def read_names
 	return [] unless File.exist?("names.txt")
 	File.read("names.txt").split("\n")
+end
+
+class LetterValidator
+	def initialize(letter, letters)
+    @letter = letter.to_s
+    @letters = letters
+  end
+
+  def valid?
+    validate
+    @message.nil?
+  end
+
+  def message
+    @message
+  end
+
+  private
+
+    def validate
+      if @letter.empty?
+        @message = "You need to enter a letter."
+      elsif @letters.include?(@letter)
+        @message = "#{@letter} is already included in our list."
+      end
+    end
 end
 
 get "/" do
  	if !session[:word]
  		hangman = Hangmanword.new()
  		session[:word] = hangman.secret_word
- 		session[:guesses] = 10
+ 		session[:guesses] = 5
  		@guesses = session[:guesses]
+ 		empty_file("names.txt")
  	end
  	@word = session[:word]
  	@guesses = session[:guesses]
@@ -31,11 +62,30 @@ end
 
 post "/" do
 	@letter = params[:letter].downcase
+	@letters = read_names
 	@word = session[:word]
 	@guesses = session[:guesses]
-	store_name("names.txt", @letter)
-	@letters = read_names
-	session[:guesses] = session[:guesses] - 1
-	@guesses = session[:guesses]
+	validator = LetterValidator.new(@letter, @letters)
+
+	if validator.valid?
+   		store_name("names.txt", @letter)
+   		@letters = read_names
+    	session[:guesses] = session[:guesses] - 1
+    	@guesses = session[:guesses]
+    	@message = "Successfully stored the letter #{@letter}."
+    	if @guesses == 0 
+    		redirect to "/lose"
+    	end
+  	else
+    	@message = validator.message
+
+  end
+
 	erb :index
+end
+
+
+get "/lose" do
+	session[:word] = nil
+	erb :lose
 end
