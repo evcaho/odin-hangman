3556dcc3788c689633de3351c8538d6eb0873b31
Evie Caldwell-Horne <evcaldwell@gmail.com>
2017-05-19 15:33:21 -0700

    initial hidden word works

--- hangman.rb
+++ hangman.rb
@@ -19,6 +19,20 @@ def read_names(filename)
 	File.read(filename).split("\n")
 end
 
+def hide_word(word, guessed_letters)
+  hidden_word = []
+  word_array = word.split("")
+  word_array.each do |letter|
+    if guessed_letters.include?(letter)
+      hidden_word.push(letter)
+    else
+      hidden_word.push("_")
+    end
+  end
+  hidden_word
+
+end
+
 def check_includes(word, letter)
   character_array = word.split("")
   if character_array.include?(letter)
@@ -64,13 +78,16 @@ get "/" do
  	@word = session[:word]
  	@guesses = session[:guesses]
  	@letter = params["letter"]
+  @correct_letters = read_names("correct.txt")
  	@letters = read_names("names.txt")
+  @hidden_word = hide_word(@word, @correct_letters)
  	erb :index
 end
 
 post "/" do
 	@letter = params[:letter].downcase
 	@letters = read_names("names.txt")
+  @correct_letters = read_names("correct.txt")
 	@word = session[:word].downcase
 	@guesses = session[:guesses]
 	validator = LetterValidator.new(@letter, @letters)
@@ -79,11 +96,13 @@ post "/" do
    		store_name("names.txt", @letter)
       if check_includes(@word, @letter)
         store_name("correct.txt", @letter)
+        @correct_letters = read_names("correct.txt")
       end
    		@letters = read_names("names.txt")
     	session[:guesses] = session[:guesses] - 1
     	@guesses = session[:guesses]
     	@message = "Successfully stored the letter #{@letter}."
+      @hidden_word = hide_word(@word, @correct_letters)
     	if @guesses == 0 
     		redirect to "/lose"
     	end
